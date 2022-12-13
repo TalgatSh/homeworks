58b94ff0c26ee87ceab97ee74f8b5de4d3eaf341
Talgat Shukataev <tshukataev@mail.ru>
2022-12-13 23:57:13 +0500

    Функции: 1 задача готова

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,5 +1,17 @@
 function getArrayParams(...arr) {
-
+  let min, max, sum, avg;
+  min = Infinity;
+  max = -Infinity;
+  sum = 0;
+  for (let i = 0; i < arr.length; i += 1) {
+    sum += arr[i];
+    if (arr[i] > max) {
+      max = arr [i]
+    } if (arr[i] < min) {
+      min = arr [i]
+    }
+  }
+  avg = Number((sum / arr.length).toFixed(2));
   return { min: min, max: max, avg: avg };
 }
 
