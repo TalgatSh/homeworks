8beca10651601c39b3cf4df87a0f79a4384da809
Talgat Shukataev <tshukataev@mail.ru>
2023-01-02 21:45:28 +0500

    Домашнее задание Массивы

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,9 @@
 function compareArrays(arr1, arr2) {
-  
+  let result;
+  return result = arr1.length === arr2.length && arr1.every((n, i) => n === arr2[i]);
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+  let result = arr.filter(gender => gender).map(midAge => midAge.age).reduce((r, i) => r + i.age / arr.length, 0.0);
+  return result;
 }
