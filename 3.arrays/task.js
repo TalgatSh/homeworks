ea85b4f9cca15bf96ac83f259faf48c7ee584d4a
Talgat Shukataev <tshukataev@mail.ru>
2023-01-03 23:33:14 +0500

    дополнения

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -8,12 +8,12 @@ function getUsersNamesInAgeRange(users, gender) {
   }; 
   if(gender != "мужской" && "женский") {
     return 0;
-  };
+  };  
   return users.filter(gen => gen.gender === "мужской").map(ages => ages.age).reduce((acc, item, index, arr) => {
     acc += item;
     if(index === arr.length - 1) {
         return acc / arr.length;
     }
     return acc;
-  });
+  });  
 }
