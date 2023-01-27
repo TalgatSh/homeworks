54ad059088ecdceea6443af3174be26a7b024170
Talgat Shukataev <tshukataev@mail.ru>
2023-01-27 20:24:14 +0500

    Асинхронность, добавление и удаление

--- 7.async/task.js
+++ 7.async/task.js
@@ -3,5 +3,26 @@ class AlarmClock {
     this.alarmCollection = [];
     intervalId = 0;
   }
-  addClock()
+  addClock(currentTime, callback) {
+    this.getCurrentFormattedTime = currentTime;
+    function startAction() {
+      callback();
+    }
+    try {
+      if (!currentTime || startAction) {
+        throw new Error("Отсутствуют обязательные аргументы");
+      }
+    } catch(error) {
+        return error;  
+    }
+    if (this.alarmCollection.includes(currentTime)) {
+      console.warn('Уже присутствует звонок на это же время');
+    } else {
+      this.alarmCollection.push(currentTime);
+    }
+    this.alarmCollection = this.alarmCollection.push({"id" : null, "time" : null, "canCall" : true});
+  }
+  removeClock(time) {
+    this.alarmCollection = this.alarmCollection.filter(alarm => alarm.time !== time)
+  }
 }
--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,6 +1,18 @@
 //Задача № 1
 function cachingDecoratorNew(func) {
-  
+  let cache = [{}];  
+  return (...args) => {
+    const hash = md5(...args);
+    const objectInCache = cache.find((item) => Object.values(item).includes(hash))
+    if (objectInCache !== undefined) {
+      return objectInCache;
+    }
+      return cache[hash];
+    
+    const result = func(...args);
+    cache[hash] = result;
+    return result;
+  }
 }
 
 //Задача № 2
