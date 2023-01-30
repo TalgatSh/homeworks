5846c99cc1df5c37e973b8cbd484d37f3491f127
Talgat Shukataev <tshukataev@mail.ru>
2023-01-30 21:57:22 +0500

    Асинхронность, последний пункт

--- 7.async/task.js
+++ 7.async/task.js
@@ -1,28 +1,48 @@
 class AlarmClock {
-  constructor(intervalId) {
+  constructor() {
     this.alarmCollection = [];
-    intervalId = 0;
+    this.intervalId = null;
   }
-  addClock(currentTime, callback) {
-    this.getCurrentFormattedTime = currentTime;
-    function startAction() {
-      callback();
-    }
-    try {
-      if (!currentTime || startAction) {
-        throw new Error("Отсутствуют обязательные аргументы");
-      }
-    } catch(error) {
-        return error;  
-    }
-    if (this.alarmCollection.includes(currentTime)) {
+  addClock(startAlarm, callback) {
+    this.startAlarm = startAlarm;
+    this.callback = callback();
+    if (!startAlarm || !callback) {
+      throw new Error("Отсутствуют обязательные аргументы");
+    }    
+    if (this.alarmCollection.some(alarm => alarm === startAlarm)) {
       console.warn('Уже присутствует звонок на это же время');
-    } else {
-      this.alarmCollection.push(currentTime);
-    }
-    this.alarmCollection = this.alarmCollection.push({"id" : null, "time" : null, "canCall" : true});
+    } 
+    this.alarmCollection.push({time : startAlarm, callback, canCall : true});
   }
-  removeClock(time) {
+  removeClock(time) {    
     this.alarmCollection = this.alarmCollection.filter(alarm => alarm.time !== time)
+  }
+  getCurrentFormattedTime() {
+    let currentTime = new Date().toLocaleTimeString("ru-Ru", {
+      hour: "2-digit",
+      minute: "2-digit",
+    });
+    return currentTime;
+  }
+  start() {
+    if (this.intervalId > 0) {
+      return;
+    }
+    this.intervalId = setInterval(this.alarmCollection.forEach(function(alarm) {
+      if (alarm === alarm.canCall) {
+        this.canCall = false;
+        return this.callback;
+      }
+    }), 1000);
+  }
+  stop() {
+    clearInterval(this.intervalId = null);
+  }
+  resetAllCalls() {
+    this.alarmCollection.forEach(reset => reset.canCall = true);
+  }
+  clearAlarms() {
+    stop.bind();
+    this.alarmCollection = []
   }
 }
