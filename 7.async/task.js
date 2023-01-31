a2d6c97a3f735e53362440dbfbfe05dbf6baca60
Talgat Shukataev <tshukataev@mail.ru>
2023-01-31 11:00:31 +0500

    Асинхронность, та же ошибка

--- 7.async/task.js
+++ 7.async/task.js
@@ -3,46 +3,44 @@ class AlarmClock {
     this.alarmCollection = [];
     this.intervalId = null;
   }
-  addClock(startAlarm, callback) {
-    this.startAlarm = startAlarm;
-    this.callback = callback();
+  addClock(startAlarm, callback) {    
     if (!startAlarm || !callback) {
       throw new Error("Отсутствуют обязательные аргументы");
     }    
-    if (this.alarmCollection.some(alarm => alarm === startAlarm)) {
+    if (this.alarmCollection.some(alarm => alarm === {startAlarm})) {
       console.warn('Уже присутствует звонок на это же время');
     } 
     this.alarmCollection.push({time : startAlarm, callback, canCall : true});
   }
   removeClock(time) {    
-    this.alarmCollection = this.alarmCollection.filter(alarm => alarm.time !== time)
+    this.alarmCollection = this.alarmCollection.filter(alarm => alarm.time !== time);
   }
   getCurrentFormattedTime() {
-    let currentTime = new Date().toLocaleTimeString("ru-Ru", {
+    return new Date().toLocaleTimeString("ru-Ru", {
       hour: "2-digit",
       minute: "2-digit",
-    });
-    return currentTime;
+    });    
   }
   start() {
-    if (this.intervalId > 0) {
+    if (this.intervalId > null) {
       return;
     }
-    this.intervalId = setInterval(this.alarmCollection.forEach(function(alarm) {
-      if (alarm === alarm.canCall) {
-        this.canCall = false;
-        return this.callback;
+    this.intervalId = setInterval(this.alarmCollection.forEach((alarm) => {
+      if (alarm === true && alarm === this.getCurrentFormattedTime()) {
+        alarm.canCall = false;
+        callback(alarm);
       }
     }), 1000);
   }
   stop() {
-    clearInterval(this.intervalId = null);
+    clearInterval(this.intervalId);
+    this.intervalId = null;
   }
   resetAllCalls() {
     this.alarmCollection.forEach(reset => reset.canCall = true);
   }
   clearAlarms() {
-    stop.bind();
-    this.alarmCollection = []
+    this.stop();
+    this.alarmCollection = [];
   }
 }
