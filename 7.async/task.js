68339d51584657192e58f1ce1320a6801d544833
Talgat Shukataev <tshukataev@mail.ru>
2023-02-02 01:30:47 +0500

    Ассинхронность, корректировка кодстайла

--- 7.async/task.js
+++ 7.async/task.js
@@ -1,34 +1,34 @@
-class AlarmClock {  
+class AlarmClock {
   constructor() {
     this.alarmCollection = [];
     this.intervalId = null;
   }
-  addClock(startAlarm, callback) {    
+  addClock(startAlarm, callback) {
     if (!startAlarm || !callback) {
       throw new Error("Отсутствуют обязательные аргументы");
-    }    
+    }
     if (this.alarmCollection.some(alarm => alarm.startAlarm === startAlarm)) {
       console.warn('Уже присутствует звонок на это же время');
-    } 
+    }
     this.alarmCollection.push({time : startAlarm, callback, canCall : true});
   }
-  removeClock(time) {    
+  removeClock(time) {
     this.alarmCollection = this.alarmCollection.filter(alarm => alarm.time !== time);
   }
   getCurrentFormattedTime() {
     return new Date().toLocaleTimeString("ru-Ru", {
       hour: "2-digit",
       minute: "2-digit",
-    });    
+    });
   }
   start() {
-    if (this.intervalId !== null) {   
+    if (this.intervalId !== null) {
       return;
     }
     this.intervalId = setInterval(() => {
       this.alarmCollection.forEach((alarm) => {
       if (alarm.time === this.getCurrentFormattedTime() && alarm.canCall === true) {
-        alarm.canCall = false;        
+        alarm.canCall = false;
         alarm.callback();
       }
     })}, 1000);
@@ -43,5 +43,5 @@ class AlarmClock {
   clearAlarms() {
     this.stop();
     this.alarmCollection = [];
-  }  
+  }
 }
